bf1e718eb2788e3c1f3cc7b674c1c26958082376
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-04-30 20:18:17 +0900

    2023/04/30 20:18

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import countriesJson from "../countries.json";
 
 const Selector = () => {
@@ -5,16 +6,27 @@ const Selector = () => {
 //	countriesJson.map((country) => {
 //		console.log(country);
 //	});
+
+	const [country, setCountry] = useState("");
+
+	const getCountryData = () => {
+		fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+			.then(res => res.json())
+			.then(data => console.log(data));
+	};
+
 	return (
 		<div>
 			<p>セレクタ</p>
-			<select>
+			<select onChange={(e) => setCountry(e.target.value)}>
+				<option>Select A Country</option>
 				{
 					countriesJson.map((country, index) =>
-						<option key={index} value="{country.Slug}">{country.Country}</option>
+						<option key={index} value={country.Slug}>{country.Country}</option>
 					)
 				}
 			</select>
+			<button onClick={getCountryData}>Get Data</button>
 		</div>
 	);
 };
