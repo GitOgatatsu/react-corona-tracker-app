b45c783de8226b0e363e751b31ab769978b5d71b
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-05-05 13:44:30 +0900

    2023/05/05 13:44

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,3 +1,4 @@
+import Header from "../components/Header";
 import Title from "../components/Title";
 import Selector from "../components/Selector";
 import Results from "../components/Results";
@@ -6,9 +7,12 @@ const TopPage = (props) => {
 //	console.log("TopPageのprops: ", props);
 	return (
 		<div className="top-page-container">
-			<Title />
-			<Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
-			<Results countryData={props.countryData} />
+			<div>
+				<Header />
+				<Title />
+				<Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
+				<Results countryData={props.countryData} />
+			</div>
 		</div>
 	);
 };
